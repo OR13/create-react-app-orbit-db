5c6cdc4767c20eabc6b40272fe16929d0f19b385
Orie Steele <orie@transmute.industries>
2018-11-12 21:38:21 -0600

    Updates

---
+++ precache-manifest.2c5bf40b2ce17f71017a2166761eddf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react-app-orbit-db/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9c098455173e0f956bc5",
+    "url": "/create-react-app-orbit-db/static/js/runtime~main.9c098455.js"
+  },
+  {
+    "revision": "3aa7133736b99bb7459f",
+    "url": "/create-react-app-orbit-db/static/js/main.3aa71337.chunk.js"
+  },
+  {
+    "revision": "2d488a479572ec3139b4",
+    "url": "/create-react-app-orbit-db/static/js/1.2d488a47.chunk.js"
+  },
+  {
+    "revision": "3aa7133736b99bb7459f",
+    "url": "/create-react-app-orbit-db/static/css/main.3ded1d76.chunk.css"
+  },
+  {
+    "revision": "577e2cf94fe4c22e28ecb2948059e756",
+    "url": "/create-react-app-orbit-db/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/create-react-app-orbit-db/precache-manifest.d0b3ed731fe5f50b017bf10ce98c69df.js"
+  "/create-react-app-orbit-db/precache-manifest.2c5bf40b2ce17f71017a2166761eddf1.js"
 );
 
 workbox.clientsClaim();
